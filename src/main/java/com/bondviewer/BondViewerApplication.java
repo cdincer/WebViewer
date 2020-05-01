0bab157839995b0e211d0d12a3a237bc11963ce2
cdincer <dincercan@gmail.com>
2020-05-01 15:34:09 +0300

    Initial Commit

---
+++ src/main/java/com/bondviewer/BondViewerApplication.java
@@ -0,0 +1,13 @@
+package com.bondviewer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BondViewerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BondViewerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bondviewer/bondviewer/BondViewerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bondviewer.bondviewer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BondViewerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
